9627fa709431bda1c28bf9a564e08a18b5eab0fb
ligi <ligi@ligi.de>
2015-04-10 14:11:20 +0200

    Cleanup

--- app/src/main/java/org/ligi/blexplorer/MainActivity.java
+++ app/src/main/java/org/ligi/blexplorer/MainActivity.java
@@ -4,12 +4,10 @@ import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothGatt;
 import android.bluetooth.BluetoothGattCallback;
-import android.bluetooth.BluetoothGattCharacteristic;
-import android.bluetooth.BluetoothGattDescriptor;
 import android.bluetooth.BluetoothGattService;
 import android.bluetooth.BluetoothManager;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
@@ -20,11 +18,7 @@ import android.widget.TextView;
 import butterknife.ButterKnife;
 import butterknife.InjectView;
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
-import java.util.SortedSet;
 import static org.ligi.blexplorer.DevicePropertiesDescriber.describeBondState;
 import static org.ligi.blexplorer.DevicePropertiesDescriber.describeType;
 
@@ -50,25 +44,25 @@ public class MainActivity extends ActionBarActivity {
 
         public DeviceViewHolder(final View itemView) {
             super(itemView);
-            ButterKnife.inject(this,itemView);
+            ButterKnife.inject(this, itemView);
         }
     }
 
-    List<BluetoothDevice> deviceMap=new ArrayList<>();
+    List<BluetoothDevice> deviceMap = new ArrayList<>();
 
     private class DeviceRecycler extends RecyclerView.Adapter<DeviceViewHolder> {
         @Override
         public DeviceViewHolder onCreateViewHolder(final ViewGroup viewGroup, final int i) {
             View v = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.foo, viewGroup, false);
-            return new DeviceViewHolder(v) ;
+            return new DeviceViewHolder(v);
         }
 
         @Override
         public void onBindViewHolder(final DeviceViewHolder deviceViewHolder, final int i) {
             final BluetoothDevice device = deviceMap.get(i);
-            
-            deviceViewHolder.mac.setText(device.getName() + " " + device.getBondState());
-            deviceViewHolder.address.setText(device.getAddress() + " " + device.getType());
+
+            deviceViewHolder.mac.setText(device.getName());
+            deviceViewHolder.address.setText(device.getAddress());
 
             deviceViewHolder.type.setText(describeType(device));
             deviceViewHolder.bondstate.setText(describeBondState(device));
@@ -79,6 +73,7 @@ public class MainActivity extends ActionBarActivity {
             return deviceMap.size();
         }
     }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -101,7 +96,7 @@ public class MainActivity extends ActionBarActivity {
                     recyclerView.getAdapter().notifyDataSetChanged();
                 }
 
-                Log.i("BLEXplorer","found " +  device.getName());
+                Log.i("BLEXplorer", "found " + device.getName());
                 device.connectGatt(MainActivity.this, false, new BluetoothGattCallback() {
                     @Override
                     public void onConnectionStateChange(final BluetoothGatt gatt, final int status, final int newState) {
@@ -114,50 +109,11 @@ public class MainActivity extends ActionBarActivity {
                     public void onServicesDiscovered(final BluetoothGatt gatt, final int status) {
                         final List<BluetoothGattService> services = gatt.getServices();
                         for (final BluetoothGattService service : services) {
-                            Log.i("BLEXplorer","found service" + service.getUuid());
+                            Log.i("BLEXplorer", "found service" + service.getUuid());
                         }
                         super.onServicesDiscovered(gatt, status);
                     }
 
-                    @Override
-                    public void onCharacteristicRead(final BluetoothGatt gatt, final BluetoothGattCharacteristic characteristic, final int status) {
-                        super.onCharacteristicRead(gatt, characteristic, status);
-                    }
-
-                    @Override
-                    public void onCharacteristicWrite(final BluetoothGatt gatt, final BluetoothGattCharacteristic characteristic, final int status) {
-                        super.onCharacteristicWrite(gatt, characteristic, status);
-                    }
-
-                    @Override
-                    public void onCharacteristicChanged(final BluetoothGatt gatt, final BluetoothGattCharacteristic characteristic) {
-                        super.onCharacteristicChanged(gatt, characteristic);
-                    }
-
-                    @Override
-                    public void onDescriptorRead(final BluetoothGatt gatt, final BluetoothGattDescriptor descriptor, final int status) {
-                        super.onDescriptorRead(gatt, descriptor, status);
-                    }
-
-                    @Override
-                    public void onDescriptorWrite(final BluetoothGatt gatt, final BluetoothGattDescriptor descriptor, final int status) {
-                        super.onDescriptorWrite(gatt, descriptor, status);
-                    }
-
-                    @Override
-                    public void onReliableWriteCompleted(final BluetoothGatt gatt, final int status) {
-                        super.onReliableWriteCompleted(gatt, status);
-                    }
-
-                    @Override
-                    public void onReadRemoteRssi(final BluetoothGatt gatt, final int rssi, final int status) {
-                        super.onReadRemoteRssi(gatt, rssi, status);
-                    }
-
-                    @Override
-                    public void onMtuChanged(final BluetoothGatt gatt, final int mtu, final int status) {
-                        super.onMtuChanged(gatt, mtu, status);
-                    }
                 });
             }
         });
