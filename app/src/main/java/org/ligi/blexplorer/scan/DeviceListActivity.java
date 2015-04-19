ab8ede011f634dbf7222e265ad00cde4651a9f1c
ligi <ligi@ligi.de>
2015-04-19 12:26:33 +0200

    Enable bluetooth if disabled

--- app/src/main/java/org/ligi/blexplorer/scan/DeviceListActivity.java
+++ app/src/main/java/org/ligi/blexplorer/scan/DeviceListActivity.java
@@ -3,6 +3,7 @@ package org.ligi.blexplorer.scan;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothManager;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.widget.LinearLayoutManager;
@@ -19,6 +20,7 @@ import org.ligi.blexplorer.R;
 
 public class DeviceListActivity extends ActionBarActivity {
 
+    private static final int REQUEST_ENABLE_BT = 2300;
     @InjectView(R.id.content_list)
     RecyclerView recyclerView;
 
@@ -47,8 +49,8 @@ public class DeviceListActivity extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_with_recycler);
 
+        setContentView(R.layout.activity_with_recycler);
         ButterKnife.inject(this);
 
         final RecyclerView.Adapter adapter = new DeviceRecycler();
@@ -56,6 +58,21 @@ public class DeviceListActivity extends ActionBarActivity {
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         recyclerView.setAdapter(adapter);
 
+        if (getBluetooth() == null || !getBluetooth().isEnabled()) {
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+        } else {
+            startScan();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(final int requestCode, final int resultCode, final Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        startScan();
+    }
+
+    private void startScan() {
         getBluetooth().startLeScan(new BluetoothAdapter.LeScanCallback() {
             @Override
             public void onLeScan(final BluetoothDevice device, final int rssi, final byte[] scanRecord) {
